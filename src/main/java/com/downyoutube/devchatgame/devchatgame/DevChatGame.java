08e7c92695d9e7090b0d942b0e791fc281d60b42
downYotube2548 <maneeohooho3790@gmail.com>
2023-09-01 21:59:25 +0700

    Initial commit

---
+++ src/main/java/com/downyoutube/devchatgame/devchatgame/DevChatGame.java
@@ -0,0 +1,17 @@
+package com.downyoutube.devchatgame.devchatgame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevChatGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
